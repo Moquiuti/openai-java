9a9a93364456e8851d98be964cec4e84094dae56
leandro.ferreira <leandro.ferreira@monitoratec.com.br>
2024-10-14 08:57:00 -0300

    Iniciando projeto de integração.

---
+++ src/main/java/br/com/alura/TestaIntegracao.java
@@ -0,0 +1,8 @@
+package br.com.alura;
+
+public class TestaIntegracao {
+    public static void main(String[] args) {
+        var user = "Gere 5 produtos";
+        var system = "Você é um gerador de produtos fictícios para um e-commerce e deve gerar apenas o nome dos produtos solicitados pelo usuário.";
+    }
+}
