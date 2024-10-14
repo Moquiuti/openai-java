5395ee5ef48cd0a639ac6308eb18305388afe633
leandro.ferreira <leandro.ferreira@monitoratec.com.br>
2024-10-14 11:29:28 -0300

    Aplicando uma boa prática desenvolvendo com prompt engineering adaptando o prompt, descrevendo o que eu quero, o que deve se considerar ou não, e dar exemplos. Tudo isso para deixar as respostas o mais consistente possível, o mais próximo do resultado esperado.

--- src/main/java/br/com/alura/CategorizadorDeProdutos.java
+++ src/main/java/br/com/alura/CategorizadorDeProdutos.java
@@ -5,15 +5,32 @@ import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
 
+import java.time.Duration;
 import java.util.List;
 
 public class CategorizadorDeProdutos {
     public static void main(String[] args) {
         final var produto = "Escova de dentes";
-        final var system = "Você é um categorizador de produtos.";
+        final var system = """
+                    Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
+                
+                    Escolha uma categoria dentra a lista abaixo:
+                
+                    1. Higiene pessoal
+                    2. Eletronicos
+                    3. Esportes
+                    4. Outros
+                
+                    ######Exemplo de uso:
+                
+                    Pergunta: Bola de futebol
+                    Resposta: Esportes
+                """;
 
         final var apiKey = System.getenv("token-api");
-        final var service = new OpenAiService(apiKey);
+
+        //Aumentar o tempo de timeout
+        var service = new OpenAiService(apiKey, Duration.ofSeconds(30));
 
         final var completionRequest = ChatCompletionRequest
                 .builder()
