23b920aca5873ead0bd5eeffc1cb4484c96d8af6
leandro.ferreira <leandro.ferreira@monitoratec.com.br>
2024-10-14 09:35:37 -0300

    Adicionado funções da biblioteca de integração, faltando apenas o token.

--- src/main/java/br/com/alura/TestaIntegracao.java
+++ src/main/java/br/com/alura/TestaIntegracao.java
@@ -1,8 +1,30 @@
 package br.com.alura;
 
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.util.List;
+
 public class TestaIntegracao {
     public static void main(String[] args) {
-        var user = "Gere 5 produtos";
-        var system = "Você é um gerador de produtos fictícios para um e-commerce e deve gerar apenas o nome dos produtos solicitados pelo usuário.";
+        final var user = "Gere 5 produtos";
+        final var system = "Você é um gerador de produtos fictícios para um e-commerce e deve gerar apenas o nome dos produtos solicitados pelo usuário.";
+
+        final var service = new OpenAiService("your_token");
+
+        final var completionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-4")
+                .messages(List.of(
+                        new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
+                ))
+                .build();
+
+        service.createChatCompletion(completionRequest)
+                .getChoices()
+                .forEach(System.out::println);
     }
 }
